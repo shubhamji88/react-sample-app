2d132a834c8cd456b99efbd2e88c10281ba8a08a
sanjeetiitr <sanjeetiitr1@gmail.com>
2021-06-14 14:57:41 +0530

    test : heroku deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const port = 4000;
+const port = process.env.BACKEND_PORT | 4000;
 app.use(express.json());
 app.use(cors());
 
